fc1766c003bc2b471bafaa8ae4accfd6fcdc60a3
MHuott <mlhuott1745@gmail.com>
2024-07-10 08:37:53 -0500

    This is the intial file for the Federated Average Main file

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -12,18 +12,79 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
-def LogisticPrediction(intercept, beta, X):
-    prediction = np.exp(intercept + np.dot(beta, X)) / (1 + np.exp(intercept + np.dot(beta, X)))
+def LogisticProbability(intercept, beta, X):
+    probability = np.exp(intercept + np.dot(beta, X)) / (1 + np.exp(intercept + np.dot(beta, X)))
 
-    return prediction
+    return probability
 
 #penalty = -(y*log(p) + (1-y)*log(1-p))
-def LogisticLoss(y, p):
-    loss = -(y * np.log(y) + (1 - y) * np.log(1 - y))
+def LogisticLoss(y, probability):
+    loss = -(y * np.log(probability) + (1 - y) * np.log(1 - probability))
 
     return loss
 
-def GradLogistic():
+def GradLogistic(prediction, X):
+    logGrad = prediction * (1 - prediction) * np.transpose(-X)
 
-    return
+    return logGrad
+
+def NewBeta(predGrad, beta, step):
+    beta = beta - step * predGrad
+
+    return beta
+
+def Train(X, y, time, step):
+    prediction = np.zeros(X.shape[0])
+    intercept = 0
+    prediction.fill(0.5)
+    beta = np.zeros(X.shape[1])
+
+    for i in range(time):
+        pred = LogisticProbability(intercept, prediction, X)
+        gradient = GradLogistic(prediction, X)
+        loss = LogisticLoss(y, pred)
+        beta = NewBeta(pred, gradient, step)
+        print(f"Iteration {i + 1}, Loss: {loss}")
+
+    return loss, prediction, intercept
+
+# Download training data from open datasets.
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+batchSize = 64
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batchSize)
+test_dataloader = DataLoader(test_data, batch_size=batchSize)
+
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
+
+# Flatten the images.
+X_flat = X.view(X.size(0), -1).numpy()
+y_numpy = y.numpy()
+
+iteration = 10000
+learningRate = 0.0001
+
+loss = Train(X_flat, y_numpy, iteration, learningRate)
+
+print(f"Final loss: {loss[0]}")
+
+
 
