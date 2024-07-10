162cc7f414374e38de31c3662120b13d4e938709
MHuott <mlhuott1745@gmail.com>
2024-07-10 15:56:13 -0500

    This is the intial file for the Federated Average Main file

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -11,80 +11,63 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+from datasets import load_dataset
+import pandas as pd
+
 
 def LogisticProbability(intercept, beta, X):
     probability = np.exp(intercept + np.dot(beta, X)) / (1 + np.exp(intercept + np.dot(beta, X)))
 
     return probability
 
+
 #penalty = -(y*log(p) + (1-y)*log(1-p))
 def LogisticLoss(y, probability):
-    loss = -(y * np.log(probability) + (1 - y) * np.log(1 - probability))
+    loss = -(np.dot(y.T, np.log(probability)) + np.dot((1 - y).T, np.log(1 - probability)))
 
     return loss
 
+
 def GradLogistic(prediction, X):
-    logGrad = prediction * (1 - prediction) * np.transpose(-X)
+    logGrad = prediction * (1 - prediction) * (-1 * np.transpose(X))
 
     return logGrad
+
 
 def NewBeta(predGrad, beta, step):
     beta = beta - step * predGrad
 
     return beta
 
+
 def Train(X, y, time, step):
     prediction = np.zeros(X.shape[0])
     intercept = 0
-    prediction.fill(0.5)
-    beta = np.zeros(X.shape[1])
+    beta = np.ones(X.shape[0])
+    print(prediction.shape)
+    print(beta.shape)
+    print(y.shape)
+    loss = 0
 
     for i in range(time):
-        pred = LogisticProbability(intercept, prediction, X)
+        probability = LogisticProbability(intercept, prediction, X)
+        print(probability.shape)
         gradient = GradLogistic(prediction, X)
-        loss = LogisticLoss(y, pred)
-        beta = NewBeta(pred, gradient, step)
+        loss = LogisticLoss(y, probability)
+        beta = NewBeta(probability, gradient, step)
         print(f"Iteration {i + 1}, Loss: {loss}")
 
     return loss, prediction, intercept
 
-# Download training data from open datasets.
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor(),
-)
+data = pd.read_csv("C:/Users/mlhuo_dkvynem/OneDrive/Desktop/Raisin_Dataset.csv", sep=',')
+data['Class']= pd.factorize(data['Class'])[0]
 
-# Download test data from open datasets.
-test_data = datasets.FashionMNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor(),
-)
+print(data)
 
-batchSize = 64
+X = data.drop('Class', axis=1)
+y = data['Class']
 
-# Create data loaders.
-train_dataloader = DataLoader(training_data, batch_size=batchSize)
-test_dataloader = DataLoader(test_data, batch_size=batchSize)
-
-for X, y in test_dataloader:
-    print(f"Shape of X [N, C, H, W]: {X.shape}")
-    print(f"Shape of y: {y.shape} {y.dtype}")
-    break
-
-# Flatten the images.
-X_flat = X.view(X.size(0), -1).numpy()
-y_numpy = y.numpy()
-
-iteration = 10000
-learningRate = 0.0001
-
-loss = Train(X_flat, y_numpy, iteration, learningRate)
-
-print(f"Final loss: {loss[0]}")
-
-
-
+print("y")
+print(y)
+print("X")
+print(X)
