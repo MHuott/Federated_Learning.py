f7f8fa1771fa43f56af6fa77dd0d83c329dca03a
MHuott <mlhuott1745@gmail.com>
2024-07-16 11:53:25 -0500

    This is the intial file for the Federated Average Main file

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -15,47 +15,60 @@ from datasets import load_dataset
 import pandas as pd
 
 
-def LogisticProbability(intercept, beta, X):
-    probability = np.exp(intercept + np.dot(X, beta)) / (1 + np.exp(intercept + np.dot(X, beta)))
+def LogisticProbability(beta, X, intercept):
+    y = np.dot(X, beta) + intercept
+
+    if y.ndim >= 1:
+        const = np.ones(X.shape) * -2
+        X = np.maximum(X, const)
+    else:
+        X = np.max(X,-2)
+
+    probability = 1 / (1 + np.exp(-y))
+    print(probability)
 
     return probability
 
 
 #penalty = -(y*log(p) + (1-y)*log(1-p))
 def LogisticLoss(y, probability):
-    loss = -(np.dot(y.T, np.log(probability)) + np.dot((1 - y).T, np.log(1 - probability)))
+    epsilon = 1e-15
 
-    return loss
+    probability =  epsilon, 1 - epsilon
+    loss = -(np.dot(y, np.log(probability)) + np.dot((1 - y), np.log(1 - probability)))
+    return loss.mean()  # Return the mean loss
+
+#intercept gradient is average difference between ground truth and probability
+def GradLogistic(probability, X, y):
+    betaGrad = np.dot((probability - y), X)
+    inteGrad = probability - y
+
+    return betaGrad, inteGrad
 
 
-def GradLogistic(prediction, X):
-    logGrad = prediction * (1 - prediction) * (-1 * np.transpose(X))
-
-    return logGrad
-
-
-def NewBeta(predGrad, beta, step):
-    beta = beta - step * predGrad
+def NewBeta(beta, gradient, step):
+    beta = beta - np.dot(step, gradient)
 
     return beta
 
+def NewIntercept(intercept, gradient, step):
+    intercept = intercept - np.dot(step, gradient)
+
+    return intercept
 
 def Train(X, y, time, step):
     prediction = np.zeros(X.shape[0])
-    intercept = 0
     beta = np.ones(X.shape[1])
-    print("Beta")
-    print(beta.shape)
-
+    beta = beta * -0.00001
+    intercept = np.ones(X.shape[0])
+    loss = 0
 
     for i in range(time):
-        probability = LogisticProbability(intercept, beta, X)
-        print("Probability")
-        print(probability.shape)
-        loss = LogisticLoss(y, probability)
-        print(probability.shape)
-        gradient = GradLogistic(prediction, X)
-        beta = NewBeta(probability, gradient, step)
+        probability = LogisticProbability(beta, X, intercept)
+        #loss = LogisticLoss(y, probability)
+        gradient = GradLogistic(probability, X, y)
+        beta = NewBeta(beta, gradient[0], step)
+        intercept = NewIntercept(intercept, gradient[1], step)
         print(f"Iteration {i + 1}, Loss: {loss}")
 
     return loss, beta, intercept
@@ -63,13 +76,7 @@ def Train(X, y, time, step):
 data = pd.read_csv("C:/Users/mlhuo_dkvynem/OneDrive/Desktop/Raisin_Dataset.csv", sep=',')
 data['Class']= pd.factorize(data['Class'])[0]
 
-print(data)
-
 X = data.drop('Class', axis=1)
 y = data['Class']
 
-print("y")
-print(y.shape)
-print("X")
-print(X.shape)
-Train(X=X, y=y, time=10, step=0.001)
+Train(X=X, y=y, time=10, step=0.0001)
